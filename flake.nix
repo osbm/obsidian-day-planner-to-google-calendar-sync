80a4c486e27da63cc57b8912370919ed4664e97a
osbm <osmanfbayram@gmail.com>
2024-11-13 23:01:57 +0300

    remove unstable nixpkgs

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = { self , nixpkgs ,... }: let
