e29f626711464b29a11e63ecfc46de4bccc836bc
osbm <osmanfbayram@gmail.com>
2024-11-13 23:49:13 +0300

    finally working flake check

--- flake.nix
+++ flake.nix
@@ -26,18 +26,24 @@
         echo 'Welcome to the development environment of the Obsidian Day Planner to Google Calendar Sync repository. (definitely needs a shorter name)'
       '';
     };
-    checks."${system}".default = pkgs.runCommandLocal "unittest" {
-      src = ./.;
-      nativeBuildInputs = with pkgs; [
+    checks."${system}".default = pkgs.stdenvNoCC.mkDerivation {
+        name = "unittest";
+        dontBuild = true;
+        src = ./.;
+        doCheck = true;
+        nativeBuildInputs = with pkgs; [
          (pkgs.python312.withPackages (ppkgs: [
             python312Packages.google-api-python-client
             python312Packages.google-auth-httplib2
             python312Packages.google-auth-oauthlib
           ]))
-      ];
-    } ''
-      python -m unittest tests/main_test.py
-    '';
-
+        ];
+        checkPhase = ''
+          python -m unittest tests/main_test.py
+        '';
+        installPhase = ''
+          mkdir "$out"
+        '';
+    };
   };
 }
